3f7f2d9b4bcf3e4150e9be19365a7455afdb961b
Thathva Sri Sai Reddy Y <34968163+thathva@users.noreply.github.com>
2019-06-16 12:14:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,25 @@
-from flask import Flask,jsonify,request
-from flask_restful import Api
-from flask_restful import Resource,reqparse
-from sklearn.externals import joblib
+from flask import Flask,request,render_template
+#from flask_restful import Api
+import joblib
 
-clf2 = joblib.load('model.pkl')
-count=joblib.load("counts.pkl")
 app = Flask(__name__)
 app.config['PROPAGATE_EXCEPTIONS'] = True
-api = Api(app)
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    query=request.get_json()
-    a = [query['Message']]
-    a=count.transform(a)
-    prediction=clf2.predict(a)
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/classify', methods=['POST'])
+def classify():
+    comment = request.form['comment']
+    x=count.transform([comment])
+    prediction=clf1.predict(x)
     if(prediction==0):
-        
-        return jsonify({'prediction':'Not Spam'})
-    
+        s="Not Spam"
     else:
-        return jsonify({'prediction':'Spam'})
-    #return jsonify(query['Message'])
-    #return jsonify({'prediction':pred})
-
+        s="Spam"
+    return render_template('result.html',pred=s)
 if __name__ == '__main__':
-    
+    clf1=joblib.load('model.pkl')
+    count=joblib.load('count.pkl')
     app.run(port=5000, debug=True)  # important to mention debug=True
